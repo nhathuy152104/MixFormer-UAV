0d67ebc5028087f31d9607ff1d965e3b6d6e4897
arsene_lupin <arsene_lupin@trunghieubh0102@gmail.com>
2026-09-18 02:25:10 +0700

    fix bug

--- lib/train/base_functions.py
+++ lib/train/base_functions.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data.distributed import DistributedSampler
 # datasets related
-from lib.train.dataset import Lasot, Got10k, MSCOCOSeq, ImagenetVID, TrackingNet, UAVAntiUAVDataset
+from lib.train.dataset import Lasot, Got10k, MSCOCOSeq, ImagenetVID, TrackingNet, UAVAntiUAV
 from lib.train.dataset import Lasot_lmdb, Got10k_lmdb, MSCOCOSeq_lmdb, ImagenetVID_lmdb, TrackingNet_lmdb
 from lib.train.data import sampler, opencv_loader, processing, LTRLoader
 import lib.train.data.transforms as tfm
