d9c315e90b64b7fec3ce3eebdf3a124f9ae645a6
arsene_lupin <arsene_lupin@trunghieubh0102@gmail.com>
2026-09-18 19:21:04 +0700

    new train

--- lib/models/mixformer2_vit/mixformer2_vit.py
+++ lib/models/mixformer2_vit/mixformer2_vit.py
@@ -302,7 +302,7 @@ def get_mixformer_vit(config, train):
 
     if config.MODEL.BACKBONE.PRETRAINED and train:
         ckpt_path = config.MODEL.BACKBONE.PRETRAINED_PATH
-        ckpt = torch.load(ckpt_path, map_location='cpu', weights_only = False)
+        ckpt = torch.load(ckpt_path, map_location='cpu', weights_only = False)['net']
         print(type(ckpt))
         new_dict = {}
         for k, v in ckpt.items():
