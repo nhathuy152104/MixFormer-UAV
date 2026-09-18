c7648a0a8e5ee967e198d363b567ee5524e41e8e
arsene_lupin <arsene_lupin@trunghieubh0102@gmail.com>
2026-09-18 19:55:16 +0700

    new train

--- lib/models/mixformer2_vit/mixformer2_vit.py
+++ lib/models/mixformer2_vit/mixformer2_vit.py
@@ -303,26 +303,10 @@ def get_mixformer_vit(config, train):
     if config.MODEL.BACKBONE.PRETRAINED and train:
         ckpt_path = config.MODEL.BACKBONE.PRETRAINED_PATH
         ckpt = torch.load(ckpt_path, map_location='cpu', weights_only = False)
-        
-        # TRÍCH XUẤT STATE_DICT TỪ BÊN TRONG CHECKPOINT
-        if 'net' in ckpt:
-            ckpt = ckpt['net']
-        elif 'model' in ckpt:
-            ckpt = ckpt['model']
-        elif 'state_dict' in ckpt:
-            ckpt = ckpt['state_dict']
-
         new_dict = {}
         for k, v in ckpt.items():
             if 'pos_embed' not in k and 'mask_token' not in k:
-                # Vẫn giữ logic cắt bỏ 'backbone.' phòng trường hợp các key bên trong 'net' có tiền tố này
-                if k.startswith('backbone.'):
-                    new_key = k.replace('backbone.', '', 1)
-                else:
-                    new_key = k
-                
-                new_dict[new_key] = v
-                
+                new_dict[k] = v
         missing_keys, unexpected_keys = vit.load_state_dict(new_dict, strict=False)
         if is_main_process():
             print("Load pretrained model from {}\n".format(ckpt_path))
@@ -330,6 +314,7 @@ def get_mixformer_vit(config, train):
             print("unexpected keys:", unexpected_keys)
             print("Loading pretrained ViT done.")
     return vit
+
 
 class MixFormer(nn.Module):
     """ Mixformer tracking with score prediction module, whcih jointly perform feature extraction and interaction. """
