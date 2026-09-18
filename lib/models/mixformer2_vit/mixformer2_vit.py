566e0ac5092c02a4586e0b7acef4e1424a077e79
arsene_lupin <arsene_lupin@trunghieubh0102@gmail.com>
2026-09-18 19:18:25 +0700

    new train

--- lib/models/mixformer2_vit/mixformer2_vit.py
+++ lib/models/mixformer2_vit/mixformer2_vit.py
@@ -302,7 +302,8 @@ def get_mixformer_vit(config, train):
 
     if config.MODEL.BACKBONE.PRETRAINED and train:
         ckpt_path = config.MODEL.BACKBONE.PRETRAINED_PATH
-        ckpt = torch.load(ckpt_path, map_location='cpu', weights_only = False)['model']
+        ckpt = torch.load(ckpt_path, map_location='cpu', weights_only = False)
+        print(type(ckpt))
         new_dict = {}
         for k, v in ckpt.items():
             if 'pos_embed' not in k and 'mask_token' not in k:
